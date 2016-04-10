6038567c0f4c50a18c0c5cebcd8a7be2be16923f
wkodate <n9lfcth9@gmail.com>
2016-04-11 01:08:30 +0900

    fix duplicated colums for app2web #17

--- app2web/app2web.sh
+++ app2web/app2web.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mysql -h localhost -u root -ppassword -e "insert into jupiter_web.items(link, title, description, date, image, rss_url) select link, title, description, date, image, rss_url from jupiter.items where DATE_ADD(date, INTERVAL 1 HOUR) > NOW(); update jupiter_web.items as i, jupiter_web.rsses as r set i.rss_id = r.id where DATE_ADD(i.date, INTERVAL 1 HOUR) > NOW()  and i.rss_url = r.rss_url;"
+mysql -h localhost -u root -ppassword -e "insert ignore into jupiter_web.items(link, title, description, date, image, rss_url) select link, title, description, date, image, rss_url from jupiter.items where DATE_ADD(date, INTERVAL 1 HOUR) > NOW(); update jupiter_web.items as i, jupiter_web.rsses as r set i.rss_id = r.id where DATE_ADD(i.date, INTERVAL 1 HOUR) > NOW()  and i.rss_url = r.rss_url;"
